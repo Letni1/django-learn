54d7bf2da8d7a5a0de2b94c058297271bea7fd07
Letni1 <letnidev@gmail.com>
2018-05-24 17:50:19 +0300

    update

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,20 +127,15 @@ USE_L10N = True
 USE_TZ = True
 
 
-# STATIC
-# ------------------------------------------------------------------------------
-# https://docs.djangoproject.com/en/dev/ref/settings/#static-root
-STATIC_ROOT = str(config['BASE_DIR'], 'staticfiles')
-# https://docs.djangoproject.com/en/dev/ref/settings/#static-url
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-# https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
+
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    str(APPS_DIR.path('static')),
-]
-# https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    os.path.join(PROJECT_ROOT, 'static'),
 ]
 
 # Simplified static file serving.
